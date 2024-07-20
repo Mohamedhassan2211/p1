57638ed427fbbbc92cba85aef35cc1c78b2ddd14
Mohamed Hassan <mohamedhassan112003@gmail.com>
2024-07-20 22:45:20 +0300

    first commit

---
+++ scripts/Courses.js
@@ -0,0 +1,23 @@
+var PROJECTS=[
+    {
+        img:"course1.png",
+        title:"FirstCourse",
+        description:"CCNA",
+    },
+    {
+        img:"course2.png",
+        title:"SecondCourse",
+        description:"OOP"
+    },
+    {
+        img:"course3.png",
+        title:"Course 3",
+        description:"Data structure",
+    },
+    {
+        img:"course4.png",
+        title:"Course 4",
+        description:"Algorithm"
+    },
+
+]
